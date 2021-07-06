b4d22d32a599f6b96d121cad27a6c912a555748d
BrunoOmoreschi <brunomoreschi.camposdesouza@gmail.com>
2021-07-06 15:23:30 -0300

    Create video_1.kt
    Vídeo mostrando função println e os tipos de variáveis numéricas e seus valores máximos bem como conversão de tipologia.

---
+++ video_1.kt
@@ -0,0 +1,19 @@
+//Codigo feito no VS Code
+
+//Função principal
+fun main() {
+    println(Int.MAX_VALUE)
+    println(Double.MAX_VALUE)
+    println(Float.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+}
+
+//Operadores de conversão:
+//toByte()
+//toShort()
+//toInt()
+//toLong()
+//toFloat()
+//toDouble()
+//toChar()
+//toString()

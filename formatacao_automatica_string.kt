bc3b762bd884975d8b454cdf9b8a4e19c22a8b61
BrunoOmoreschi <brunomoreschi.camposdesouza@gmail.com>
2021-07-06 17:34:06 -0300

    Create formatacao_automatica_string.kt
    exemplo de como fazer print  de strings com espaços automáticos.

---
+++ formatacao_automatica_string.kt
@@ -0,0 +1,11 @@
+//Código feito no VS Code
+
+//Para concatenar strings com espaços altomaticos deve-se usar "S{x}" - x é a váriavel ou string chamada
+// Exemplo:
+fun main() {
+    val name = "Aline"
+    val adjetivo = "linda"
+
+    println ("${name} ${adjetivo}!")    
+}
+

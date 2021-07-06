1a1a46595cc0de572f1c4ea6e7832c37a9c32b30
BrunoOmoreschi <brunomoreschi.camposdesouza@gmail.com>
2021-07-06 16:17:09 -0300

    Create nullability.kt
    Explicação sobre nulabilidade e como deve ser declarada.

---
+++ nullability.kt
@@ -0,0 +1,9 @@
+//Escrito no VS Code
+
+//Nullability é a capacidade de váriaveis serem nulas, mas apenas quando o programador define assim e não pelo usuario, por isso, null safe.
+// A notação é o ponto de interrogação na declaração da variavel.
+
+//Exemplo:
+
+var month:Int? = null
+//Declara que existe uma variavel chamada month que é do tipo inteiro e que pode ter seu valor nulo e depois atribui o valor nulo.

20bf85460ddb7b8f1d1eb764c7fa44268270453d
BrunoOmoreschi <brunomoreschi.camposdesouza@gmail.com>
2021-07-06 17:54:21 -0300

    Update formatacao_automatica_string.kt
    Add. função para teste de captalize. e descrição de outros métodos de tratamento de string.

--- formatacao_automatica_string.kt
+++ formatacao_automatica_string.kt
@@ -2,10 +2,22 @@
 
 //Para concatenar strings com espaços altomaticos deve-se usar "S{x}" - x é a váriavel ou string chamada
 // Exemplo:
-fun main() {
-    val name = "Aline"
-    val adjetivo = "linda"
+//fun main() {
+//    val name = "Aline"
+//  val adjetivo = "linda"
+//
+//  println ("${name} ${adjetivo}!")    
+//}
 
-    println ("${name} ${adjetivo}!")    
+//Função com exemplo do captalize: Retorna a primeira letra do nome com ctz maiúscula
+fun main(){
+    val welcome = "Bem vindo(a)"
+    var name: String = "bruno"
+
+    println("${welcome} ${name.capitalize()}")
 }
 
+//Outros métodos de formatação:
+//toUpperCase() / toLowerCase() / decaptalize()
+//Remoção de espaços nas extremidades:: trimEnd() / trimStart() / trim()
+//Troca de caracteres:: replace(x,y) x o que estou tirando, y o que estou colocando no lugar
